f28acfc7b1b1fc41b2755e9075ea5e58e029ea63
凌 大少 <smling9_9@hotmail.com>
2017-06-09 11:20:47 +0800

    Add file to Git.

---
+++ Home.Clock/src/WeatherService/WeatherUndergroundService.ts
@@ -0,0 +1,26 @@
+﻿class WeatherUndergroundService implements IWeatherService {
+
+    protected _rootPath: string = "http://api.wunderground.com/api/";
+    protected _apiKey: string = "f1439c571fe4a431";
+
+    // Get Condition by location. 
+    // Sample: http://api.wunderground.com/api/f1439c571fe4a431/conditions/q/22.32,114.18.json
+    protected _conditionPath: string = this._rootPath + this._apiKey + "/conditions/q/{lat},{long}.json";
+
+    findCurrentObservation(latitude: number, longitude: number) {
+        // Get Web service path.
+        let _requestUrl: string = this._conditionPath.replace("{lat}", latitude.toString()).replace("{long}", longitude.toString());;
+
+        // Get result from Web API call.
+        jQuery.getJSON(_requestUrl).done(function (data: any) {
+            alert(data);
+        }).fail(function (data: DOMException) {
+            alert(data.message);
+        });
+    }
+    
+    // Not support related weather warning.
+    findWeatherWarnings() {
+        return null;
+    }
+}
